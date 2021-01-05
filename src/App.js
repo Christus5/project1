24fa0e259f9af318c9ee68fad9c557776b218ccc
Luka Tavkhelidze <52665503+Christus5@users.noreply.github.com>
2021-01-05 13:36:41 +0400

    level 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,81 @@
+import React from 'react';
 
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className={'square'}
+        onClick={() => this.setState({ value: 'X' })}
+      >
+        {this.state.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  renderSquare(i) {
+    return (<Square value={i} />);
+  }
+
+  render() {
+    const status = 'Next player: X'
+
+    return (
+      <div>
+        <div className={'status'}>{status}</div>
+        <div className={'board-row'}>
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className={'board-row'}>
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className={'board-row'}>
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        <div className={'game-info'}>
+          <div></div>
+          <ol></ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className={'game'}>
+        <div className={'game-board'}>
+          <Board />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
